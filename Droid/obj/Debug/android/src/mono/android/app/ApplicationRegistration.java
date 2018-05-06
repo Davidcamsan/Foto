a9fb7ae796e2d79642fe613dc00c83b5f02c01c4
David Cambronero <Davidcamsan16@gmail.com>
2018-05-06 10:10:10 -0600

    Initial commit

---
+++ Droid/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("Foto.Droid.MainApplication, Foto.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md5ed80e9cbe7be6fc10e03c68ed4e1ed5f.MainApplication.class, md5ed80e9cbe7be6fc10e03c68ed4e1ed5f.MainApplication.__md_methods);
+		
+	}
+}
---
+++ Foto/FotoPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Xamarin.Forms;
+using System;
+using Plugin.Media;
+
+namespace Foto
+{
+    public partial class FotoPage : ContentPage
+    {
+        public FotoPage()
+        {
+            InitializeComponent();
+            CameraButton.Clicked += CameraButton_Clicked;
+        }
+
+        private async void CameraButton_Clicked(object sender, EventArgs e)
+        {
+
+
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                DisplayAlert("No Camera", ":( No camera available.", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                Directory = "Sample",
+                Name = "test.jpg"
+            });
+
+            if (file == null)
+                return;
+
+            await DisplayAlert("File Location", file.Path, "OK");
+
+            PhotoImage.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                return stream;
+            });
+        }
+    }
+}
